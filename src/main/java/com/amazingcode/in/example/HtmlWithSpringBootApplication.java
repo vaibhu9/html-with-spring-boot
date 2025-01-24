87182d8348bbe8ee91d35ba7d9ccf91dee018604
vaibhu9 <vaibhavshejol9@gmail.com>
2025-01-24 16:35:56 +0530

    project added

---
+++ src/main/java/com/amazingcode/in/example/HtmlWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.amazingcode.in.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlWithSpringBootApplication.class, args);
+	}
+
+}
